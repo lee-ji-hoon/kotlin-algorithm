291cc25e0667b24d4990d77abb307afbc3fb33a8
lee-ji-hoon <dlwlgns1240@naver.com>
2022-09-14 01:02:23 +0900

    Solve: 7576 tomato

---
+++ 0x09_BFS/3_7576_tomato.kt
@@ -0,0 +1,68 @@
+import java.util.*
+
+private val dx = intArrayOf(1, 0, -1, 0)
+private val dy = intArrayOf(0, 1, 0, -1)
+
+private class Node(var x: Int, var y: Int)
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (M, N) = readLine().split(" ").map { it.toInt() }
+
+    val tomatoList = Array(N) { IntArray(M) } // 입력 받은 토마토
+    val dist = Array(N) { IntArray(M) } // 익었는지 확인하기 위한 배열
+
+    for (i in 0 until N) {
+        val token = StringTokenizer(readLine())
+        for (j in 0 until M) {
+            tomatoList[i][j] = token.nextToken().toInt()
+            dist[i][j] = -1 // 익었을 경우 0으로 초기화 해주기 위해 기본 초기화 상태 바꾸기
+        }
+    }
+    close()
+    // 1. 익은 토마토 전부 Queue에 담아주기
+    val q = findTomato(N, M, tomatoList, dist)
+    solution(N, M, tomatoList, dist, q)
+}
+
+private fun findTomato(N: Int, M: Int, tomato: Array<IntArray>, dist: Array<IntArray>): Queue<Node> {
+    val queue: Queue<Node> = LinkedList()
+    for (i in 0 until N) for (j in 0 until M)  // 2. 익은 토마토 담고 dist[i][j] 배열은 0으로 초기화
+        if (tomato[i][j] == 1) {
+            queue.offer(Node(i, j))
+            dist[i][j] = 0
+        }
+    return queue
+}
+
+private fun solution(N: Int, M: Int, tomato: Array<IntArray>, dist: Array<IntArray>, q: Queue<Node>) {
+    var max = 0
+    while (!q.isEmpty()) {
+        val cur = q.poll()
+        for (i in 0..3) {
+            val nx = cur.x + dx[i]
+            val ny = cur.y + dy[i]
+
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue
+            // 익지 않은 토마토를 찾아야 하므로 tomato 배열의 값이 0이 아니거나 dist 배열의 값이 -1(익지 않은 토마토)이 아닌 경우 패스
+            if (dist[nx][ny] != -1 || tomato[nx][ny] != 0) continue
+            dist[nx][ny] = dist[cur.x][cur.y] + 1
+            q.offer(Node(nx, ny))
+            max = max.coerceAtLeast(dist[nx][ny])
+        }
+    }
+    // 3. 익지 않은 토마토가 있는지 확인
+    if (!isCheckedTomato(N, M, dist, tomato)) {
+        println(-1)
+    } else {
+        println(max)
+    }
+}
+
+private fun isCheckedTomato(N: Int, M: Int, dist: Array<IntArray>, tomato: Array<IntArray>): Boolean {
+    for (i in 0 until N) for (j in 0 until M) {  // dist 배열(익는데 걸리는 시간 값이 -1(초기화 상태) 면서
+        if (dist[i][j] == -1 && tomato[i][j] == 0) { // tomato 배열에서 == 0(익지 않은) 경우 익지 않은 토마토 존재
+            return false
+        }
+    }
+    return true
+}
---
+++ 0x09_BFS/record.md
@@ -0,0 +1,1 @@
+<img width="1147" alt="image" src="https://user-images.githubusercontent.com/53300830/189950281-8589725d-40bf-41d8-b070-ac916279dd56.png">
