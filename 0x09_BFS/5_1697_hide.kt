735131c71541a087794a3017c033040734ff3655
lee-ji-hoon <dlwlgns1240@naver.com>
2022-09-16 00:29:04 +0900

    Solve : 1697 hide

---
+++ 0x09_BFS/5_1697_hide.kt
@@ -0,0 +1,35 @@
+import java.util.*
+
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (N, K) = readLine().split(" ").map { it.toInt() }
+    val dist = IntArray(100_001)
+    solution(N, K, dist)
+    println(dist[K])
+}
+
+private fun solution(n: Int, k: Int, dist: IntArray) {
+    val q: Queue<Int> = LinkedList()
+    q.offer(n)
+    // 1. 3가지 방향 확인 [-1, +1, *2]
+    while (!q.isEmpty()) {
+        val cur = q.poll()
+        // 2. 만약 이동한 방향의 값이 동생 위치와 동일하다면 answer = arr[nx];
+        if (cur == k)
+            break
+        val max = dist.size
+
+        if (cur + 1 < max && dist[cur + 1] == 0) {
+            dist[cur + 1] = dist[cur] + 1
+            q.offer(cur + 1)
+        }
+        if (cur - 1 >= 0 && dist[cur - 1] == 0) {
+            dist[cur - 1] = dist[cur] + 1
+            q.offer(cur - 1)
+        }
+        if (cur * 2 < max && dist[cur * 2] == 0) {
+            dist[cur * 2] = dist[cur] + 1
+            q.offer(cur * 2)
+        }
+    }
+}
--- 0x09_BFS/record.md
+++ 0x09_BFS/record.md
@@ -1,2 +1,4 @@
 <img width="1147" alt="image" src="https://user-images.githubusercontent.com/53300830/189950281-8589725d-40bf-41d8-b070-ac916279dd56.png">  
 <img width="1145" alt="image" src="https://user-images.githubusercontent.com/53300830/190174666-b87f575f-763d-42fa-8f3c-ab54402cac70.png">
+<img width="1148" alt="image" src="https://user-images.githubusercontent.com/53300830/190444564-06816cb4-7781-4891-a64f-a9aed70bc2ce.png">
+
