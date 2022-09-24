b712785fd69c47bb8d49c7a15d18c830c45d94ca
lee-ji-hoon <dlwlgns1240@naver.com>
2022-09-24 19:31:57 +0900

    잘못된 주석 제거

--- 0x0c_BackTracking/10_15657_N10.kt
+++ 0x0c_BackTracking/10_15657_N10.kt
@@ -16,7 +16,7 @@ fun main() = with(System.`in`.bufferedReader()) {
 
 private fun solution(depth: Int, start:Int, n: Int, m: Int) {
     if (depth == m) {
-        // 3. 같은 조합이 들어올수가 없으므로 set으로 걸러주기
+        // 3. 출력하기 위해서 StirngBuilder에 누적
         for(s in answer) {
             sb.append(s).append(" ")
         }
