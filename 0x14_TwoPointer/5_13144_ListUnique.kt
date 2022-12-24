7f6c268a2abda150402cef8174984e881f0f152d
lee-ji-hoon <dlwlgns1240@naver.com>
2022-12-24 13:24:51 +0900

    Solve : 13144 ListUnique

---
+++ 0x14_TwoPointer/5_13144_ListUnique.kt
@@ -0,0 +1,26 @@
+import java.util.*
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+
+    val st = StringTokenizer(readLine(), " ")
+    val arr = Array(n) { st.nextToken().toInt() }
+    var isVisited = BooleanArray(100_001)
+
+    var end = 0
+    var answer = 0L
+
+    // start부터 시작해서 같은 숫자가 나올때까지 end 증가
+    // end가 더 갈 수 없다면 end - start 값을 누적
+    // 1,2,3,4,5 라면 start는 0일때 end가 5까지 진행
+    // 1, 12, 123, 1234, 12345 5개 수열이 생성될 수 있다.
+    for (start in 0 until n) {
+        while (end < n) {
+            if (isVisited[arr[end]]) break
+            isVisited[arr[end++]] = true
+        }
+        answer = answer + (end - start)
+        isVisited[arr[start]] = false
+    }
+    println(answer)
+}
--- out/production/kotlin-algorithm/record.md
+++ out/production/kotlin-algorithm/record.md
@@ -6,6 +6,12 @@
 
 <img width="1142" alt="image" src="https://user-images.githubusercontent.com/53300830/209041532-6a5fc3b2-4f36-42b0-a8ca-b01ca681d957.png">
 
+
 ### 3번
 
 <img width="1147" alt="image" src="https://user-images.githubusercontent.com/53300830/209253066-a1aa9fd0-5ee6-4770-8f61-7daea4135db7.png">
+
+
+### 4번
+
+<img width="1143" alt="image" src="https://user-images.githubusercontent.com/53300830/209253976-4cad26c7-7147-47cc-8594-b86101cda130.png">
